1a981fb2288d60f513f3056a87c079580007cc94
gokayoncu <gokayoncu35@gmail.com>
2025-03-08 16:20:25 +0300

    Folder edit

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// View Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.status(200).render("index", { pageName: "index" });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", { pageName: "about" });
+});
+app.get("/courses", (req, res) => {
+  res.status(200).render("courses", { pageName: "courses" });
+});
+app.get("/dashboard", (req, res) => {
+  res.status(200).render("dashboard", { pageName: "dashboard" });
+});
+app.get("/contact", (req, res) => {
+  res.status(200).render("contact", { pageName: "contact" });
 });
 
 app.listen(port, () => {
